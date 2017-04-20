73279c32c2e9797432f657e60d183481c7954d68
Chen <chennsh5@mail2.sysu.edu.cn>
2017-04-21 01:05:50 +0800

    增加了补间动画（透明度）

--- app/src/main/java/com/example/chen/androidanimation/MainActivity.java
+++ app/src/main/java/com/example/chen/androidanimation/MainActivity.java
@@ -4,6 +4,8 @@ import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 
 /**
@@ -12,12 +14,16 @@ import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
     ImageView frame_animation_view;
+    ImageView alpha_animation_view;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         setContentView(R.layout.activity_main);
         frame_animation_view = (ImageView) findViewById(R.id.FrameAnimation);
         AnimationDrawable animationDrawable = (AnimationDrawable)frame_animation_view.getBackground();
         animationDrawable.start();
+        alpha_animation_view = (ImageView) findViewById(R.id.AlphaAnimation);
+        AlphaAnimation alphaAnimation = (AlphaAnimation) AnimationUtils.loadAnimation(this, R.anim.alpha_animation);
+        alpha_animation_view.setAnimation(alphaAnimation);
         super.onCreate(savedInstanceState);
     }
 }
